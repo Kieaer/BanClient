98b5bb5e542267d03f1a9982db7850aac82ad717
키에르 <44261958+Kieaer@users.noreply.github.com>
2019-10-22 23:28:43 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# BanClient
+Source code from Essentials!<br>
+When the player connects, the plugin will checks if the banned this player from main server, and if it's true, player will kicked from server.
+
+**For security reason, the IP of the server using this plugin is sent to the main server.**
+
+## Installation
+
+Put this plugin in the ``<server folder location>/config/plugins`` folder.
---
+++ src/main/java/kieaer/banclient/Main.java
@@ -0,0 +1,96 @@
+package kieaer.banclient;
+
+import io.anuke.arc.Events;
+import io.anuke.arc.util.CommandHandler;
+import io.anuke.arc.util.Log;
+import io.anuke.mindustry.Vars;
+import io.anuke.mindustry.game.EventType.PlayerJoin;
+import io.anuke.mindustry.gen.Call;
+import io.anuke.mindustry.plugin.Plugin;
+import org.json.JSONArray;
+import org.json.JSONTokener;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+import static io.anuke.mindustry.Vars.netServer;
+
+public class Main extends Plugin{
+    public Main(){
+        Events.on(PlayerJoin.class, e -> {
+            Thread t = new Thread(() -> {
+                try {
+                    InetAddress address = InetAddress.getByName("192.168.35.100");
+                    Socket socket = new Socket(address, 25000);
+                    OutputStream os = socket.getOutputStream();
+                    OutputStreamWriter osw = new OutputStreamWriter(os, StandardCharsets.UTF_8);
+                    BufferedWriter bw = new BufferedWriter(osw);
+
+                    bw.write("[]\n");
+                    bw.flush();
+
+                    InputStream is = socket.getInputStream();
+                    InputStreamReader isr = new InputStreamReader(is, StandardCharsets.UTF_8);
+                    BufferedReader br = new BufferedReader(isr);
+                    String message = br.readLine();
+
+                    is.close();
+                    isr.close();
+                    br.close();
+                    socket.close();
+                    JSONTokener ar = new JSONTokener(message);
+                    JSONArray result = new JSONArray(ar);
+
+                    boolean kick = false;
+
+                    for (int i = 0; i < result.length(); i++) {
+                        String[] array = result.getString(i).split("\\|", -1);
+                        if (array[0].length() == 12) {
+                            netServer.admins.banPlayerID(array[0]);
+                            if (e.player.uuid.equals(array[0])) {
+                                kick = true;
+                            }
+                            if (!array[1].equals("<unknown>") && array[1].length() <= 15) {
+                                if (Vars.netServer.admins.getInfo(e.player.uuid).lastIP.equals(array[1])) {
+                                    kick = true;
+                                }
+                            }
+                        }
+                        if (array[0].equals("<unknown>")) {
+                            if (Vars.netServer.admins.getInfo(e.player.uuid).lastIP.equals(array[1])) {
+                                kick = true;
+                            }
+                        }
+                    }
+                    if (kick) {
+                        Call.onKick(e.player.con, "You're banned from main server!");
+                        Log.info(e.player.name + " player has been kicked due to banned from main server.");
+                    } else {
+                        Log.info(e.player.name + " player isn't banned from main server.");
+                    }
+                    result = null;
+                    ar = null;
+                    System.gc();
+                } catch (Exception ex) {
+                    if(Objects.equals(ex.getMessage(), "Connection refused: connect")){
+                        Log.err("Can't connect to main server!");
+                    } else {
+                        ex.printStackTrace();
+                    }
+                }
+            });
+            t.start();
+        });
+    }
+
+    @Override
+    public void registerServerCommands(CommandHandler handler) {
+    }
+
+    @Override
+    public void registerClientCommands(CommandHandler handler) {
+    }
+}
